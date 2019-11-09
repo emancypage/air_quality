902bc02016f670b748416b3ed509136326b627aa
Szymon Szymkowiak <szymkowiak.szymon@gmail.com>
2019-11-09 16:13:31 +0100

    Remove unnecessary exception

--- src/Service/AirQualityRestApi.php
+++ src/Service/AirQualityRestApi.php
@@ -4,18 +4,15 @@
 namespace App\Service;
 
 
-use App\Exception\WrongStationIdException;
 use Symfony\Component\HttpClient\HttpClient;
 
 
 class AirQualityRestApi
 {
-    const PERMITTED_STATION_IDS = ['114', '117', '129'];
 
     /**
      * @param string $stationId
      * @return string
-     * @throws WrongStationIdException
      * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
      * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
      * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
@@ -24,20 +21,12 @@ class AirQualityRestApi
      */
     public function getCurrentStatus(string $stationId) : string
     {
-        if(!$this->validateStation($stationId)) {
-            throw new WrongStationIdException('Wrong station id passed.');
-        }
         $client = HttpClient::create();
         $response = $client->request('GET', 'http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/'.$stationId);
 
         $stationData = $response->toArray();
 
         return $stationData['stIndexLevel']['indexLevelName'];
-    }
-
-    private function validateStation($stationId)
-    {
-        return in_array($stationId, self::PERMITTED_STATION_IDS);
     }
 
     /**
