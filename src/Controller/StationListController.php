97bc2406c062ca63e7127510fa995a4152aaa8cf
Szymon Szymkowiak <szymkowiak.szymon@gmail.com>
2019-11-11 15:54:50 +0100

    Add synch action

--- src/Controller/StationListController.php
+++ src/Controller/StationListController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 use App\Service\AirQualityRestApi;
 use App\Service\ApiSync;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -14,12 +16,24 @@ class StationListController extends AbstractController
     /**
      * @Route("/station-list", name="station_list")
      */
-    public function index(AirQualityRestApi $airQualityRestApi, ApiSync $apiSync)
+    public function index(
+        AirQualityRestApi $airQualityRestApi,
+        ApiSync $apiSync,
+        Request $request
+    )
     {
-        $apiSync->syncStationList();
+        $form = $this->createFormBuilder()
+            ->add('sync', SubmitType::class, ['label' => 'Sync'])
+            ->getForm();
 
-        return $this->render('air_quality/index.html.twig', [
-            'stationList' => $airQualityRestApi->getStationList()
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $apiSync->syncStationList();
+        }
+
+        return $this->render('air_quality/station_list.html.twig', [
+            'stationList' => $airQualityRestApi->getStationList(),
+            'form' => $form->createView()
         ]);
     }
 }
